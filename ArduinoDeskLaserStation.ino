a1f32431c2589d8a78b81e1275b801f187c19d14
thomasc <thomas.chemin@linkt.fr>
2018-10-12 18:36:45 +0200

    feat led, lcd, opti code

--- ArduinoDeskLaserStation.ino
+++ ArduinoDeskLaserStation.ino
@@ -12,12 +12,31 @@ Author:     LINKT\thomasc
 //
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
 #include "var.h"
 
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PINCOLOR, NEO_GRB + NEO_KHZ800);
+int j = 0;
+String text = "";
+LiquidCrystal_I2C lcd(LCDADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 void setup()
 {
 	lcd.begin(16, 2);
-	;	pinMode(int(LASER), OUTPUT);
+	lcd.home();// go home
+	lcd.print("DEBUT");
+	pixels.begin();
+	pixels.setBrightness(BRIGHTNESS);
+	ledColor(60, 180, 150, 0);
+	ledColor(0, 0, 0, 0);
+	delay(50);
+	ledColor(0, 180, 150, 0);
+	delay(100);
+	ledColor(0, 0, 0, 0);
+	delay(100);
+	ledColor(0, 180, 150, 0);
+
+	pinMode(int(LASER), OUTPUT);
 	j = 0;
 
 	Serial.begin(SERIALBAUDS);
@@ -31,6 +50,14 @@ void setup()
 
 
 }
+void ledColor(int time, int r, int v, int b) {
+	for (int i = 0;i < NUMPIXELS;i++) {
+		pixels.setPixelColor(i, pixels.Color(r, v, b)); // Moderately bright green color.
+
+		pixels.show();
+		delay(time);
+	}
+}
 void zero()
 {
 	digitalWrite(int(LASER), LOW);
@@ -42,7 +69,8 @@ void one()
 	digitalWrite(int(LASER), HIGH);
 
 }
-int recepteur(int i, int cible, bool tab[]) {
+
+int recepteur(int i, int cible, bool tab[13]) {
 	if (i < 7) {
 		if (digitalRead(cible)) {
 			//si 1 increment
@@ -61,33 +89,45 @@ int recepteur(int i, int cible, bool tab[]) {
 	return i;
 
 }
+void ecriture(bool tab[13], String cible, int j) {
+	//si ensemble trouve affichage
+	for (size_t i = 0; i < 13; i++)
+	{
+		//Serial.println(char(tab[i]));
+		//concat entete +id en string
+		text.concat(String(tab[i]));
+	}
 
+	//ecriture log
+	lcd.home();// go home
+	lcd.print(text);
+	lcd.setCursor(0, 1);
+	lcd.print(cible);
+	text = "";
+	zero();
+	ledColor(0, 0, 0, 0);
+	ledColor(int(TIME), 180, 150, 0);
+	lcd.clear();
+	
+}
 // Add the main program code into the continuous loop() function
 void loop()
 {
-
 	i_C_AVANT = recepteur(i_C_AVANT, int(C_AVANT), tab_C_AVANT);
 	i_C_EPAULE = recepteur(i_C_EPAULE, int(C_EPAULE), tab_C_EPAULE);
 	i_C_PISTOLET = recepteur(i_C_PISTOLET, int(C_PISTOLET), tab_C_PISTOLET);
 
 	if (i_C_AVANT == 13) {
-		//si ensemble trouve affichage
-
-		for (size_t i = 0; i < 13; i++)
-		{
-			//concat entete +id en string
-			text.concat(String(tab_C_AVANT[i]));
-		}
-		//ecriture log
-		lcd.home();                   // go home
-		lcd.print(text);
-		text = "";
+		ecriture(tab_C_AVANT, " cible avant", i_C_AVANT);
 		i_C_AVANT = 0;
-		zero();
-		delay(int(TIME));
-		lcd.home();                   // go home
-		lcd.print("");
-		delay(int(TIME));
+	}
+	else if (i_C_EPAULE == 13) {
+		ecriture(tab_C_EPAULE, " cible epaule", i_C_EPAULE);
+		i_C_EPAULE = 0;
+	}
+	else if (i_C_PISTOLET == 13) {
+		ecriture(tab_C_PISTOLET, " cible pistolet", i_C_PISTOLET);
+		i_C_PISTOLET = 0;
 	}
 	else {
 		if (j > 12) { j = 0; }
--- var.h
+++ var.h
@@ -2,6 +2,11 @@
 #define SERIALBAUDS 9600
 
 
+//Led
+#define PINCOLOR 3
+#define NUMPIXELS 16
+#define BRIGHTNESS 55
+
 //cible avant
 static int const C_AVANT = 5;
 int i_C_AVANT = 0;
@@ -20,13 +25,9 @@ bool tab_C_PISTOLET[13] = { 1,1,1,1,1,1,1,0,0,0,0,0,0 };
 //laser
 #define PULSE 50;
 static int const LASER = 4;
-static int const TIME = 4000;
-int j = 0;
+static int const TIME = 500;
 bool tabID[13] = { 1,1,1,1,1,1,1,0,0,0,1,1,0 };
 
-
-
 //lcd
-String text = "";
 #define BACKLIGHT_PIN 13
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+#define LCDADDR 0x27
